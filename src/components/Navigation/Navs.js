4f386638bf0bf5ea117f21c6ef84d76a9264fb3f
Özgür Çiriş <ozgurciris@gmail.com>
2021-03-17 20:28:54 +0300

    initial create gif project

---
+++ src/components/Navigation/Navs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navs() {
+    return (
+        <header>
+            <div className="logo">
+                <h1>JURASIC PARK</h1>
+            </div>
+        </header>
+    )
+}
